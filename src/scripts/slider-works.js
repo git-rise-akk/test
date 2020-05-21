c6d7a99859c649365b669800fef434ecaea3778e
Aleksander Tkachev <bobor9752@gmail.com>
2020-05-21 15:19:25 +0300

    ДЗ 2 исправление паралакса 2

--- src/scripts/slider-works.js
+++ src/scripts/slider-works.js
@@ -1,25 +1,41 @@
 import Vue from "vue";
 
 const btns = {
-    template: "#slider-btns"
+    template: "#slider-btns",
 };
 
 const thumbs = {
-    template: "#slider-thumbs"
+    template: "#slider-thumbs",
+    props: ["works", "currentWork"],
 };
 
 const display = {
     template: "#slider-display",
-    components: {thumbs, btns}
+    components: {thumbs, btns},
+    props: ["currentWork", "works"],
+    computed: {
+        reversedWorks() {
+            const works = [...this.works];
+            return works.reverse();
+        }
+    }
+    
 };
 
 const tags = {
-    template: "#slider-tags"
+    template: "#slider-tags",
+    props: ["tags"]
 };
 
 const info = {
     template: "#slider-info",
     components: {tags},
+    props: ["currentWork"],
+    computed: {
+        tagsArray() {
+            return this.currentWork.skills.split(",")
+        }
+    }
 };
 
 new Vue({
@@ -28,8 +44,51 @@ new Vue({
     components: {display, info},
     data() {
         return {
-            works: []
-        }   
+            works: [],
+            currentIndex: 0,
+        };   
     },
-    
+
+    computed: {
+        currentWork() {
+            return this.works[this.currentIndex]
+        },
+    },
+    watch: {
+        currentIndex(value) {
+            this.makeInfiniteLoopForIndex(value);
+        },
+    },
+    methods: {
+        makeInfiniteLoopForIndex(value) {
+            const worksAmountFromZero = this.works.length -1;
+            if (value > worksAmountFromZero) this.currentIndex = 0;
+            if (value < 0) this.currentIndex = worksAmountFromZero;
+        },
+        handleSlide(direction) {
+            switch(direction) {
+                case "next":
+                    this.currentIndex++;
+                    break;
+                case "prev":
+                    this.currentIndex--;
+                    break;
+            }
+                
+        },
+        makeArrWithRequireImages(array) {
+            return array.map(item => {
+                const requirePic = require(`../images/content/${item.photo}`);
+                item.photo = requirePic;
+
+                return item;
+            });
+        },
+    },
+
+    created() {
+        const data = require("../data/works.json");
+        this.works = this.makeArrWithRequireImages(data);
+        this.currentWork = this.works[this.currentIndex];
+    }
 });
